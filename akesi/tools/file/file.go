b763599e0112f0781352ef56c77e9e81d835f691
Stephen Malone <mail@wirehaiku.org>
2024-06-22 13:18:02 +1000

    Added tools/file package.

---
+++ akesi/tools/file/file.go
@@ -0,0 +1,108 @@
+// Package file implements file system access functions.
+package file
+
+import (
+	"errors"
+	"fmt"
+	"io/fs"
+	"os"
+	"path/filepath"
+	"sort"
+	"strings"
+
+	"github.com/stvmln86/akesi/akesi/tools/path"
+)
+
+// Create writes a new file with a string..
+func Create(dest, body string, mode os.FileMode) error {
+	if Exists(dest) {
+		return fmt.Errorf("cannot create file %q: already exists", dest)
+	}
+
+	if err := os.WriteFile(dest, []byte(body), mode); err != nil {
+		return fmt.Errorf("cannot create file %q: %w", dest, err)
+	}
+
+	return nil
+}
+
+// Delete renames a file to the ".trash" file extension.
+func Delete(orig string) error {
+	if !Exists(orig) {
+		return fmt.Errorf("cannot delete file %q: does not exist", orig)
+	}
+
+	dire := path.Dire(orig)
+	name := path.Name(orig)
+	dest := path.Join(dire, name, ".trash")
+	if err := os.Rename(orig, dest); err != nil {
+		return fmt.Errorf("cannot delete file %q: %w", orig, err)
+	}
+
+	return nil
+}
+
+// Exists returns true if a file path exists.
+func Exists(orig string) bool {
+	_, err := os.Stat(orig)
+	return !errors.Is(err, fs.ErrNotExist)
+}
+
+// List returns all paths in a directory matching an extension.
+func List(dire, extn string) ([]string, error) {
+	if !Exists(dire) {
+		return nil, fmt.Errorf("cannot list directory %q: does not exist", dire)
+	}
+
+	pttn := filepath.Join(dire, "*"+extn)
+	elems, err := filepath.Glob(pttn)
+	if err != nil {
+		return nil, fmt.Errorf("cannot list directory %q: %w", dire, err)
+	}
+
+	sort.Strings(elems)
+	return elems, nil
+}
+
+// Read returns a file's body as a string.
+func Read(orig string) (string, error) {
+	if !Exists(orig) {
+		return "", fmt.Errorf("cannot read file %q: does not exist", orig)
+	}
+
+	bytes, err := os.ReadFile(orig)
+	if err != nil {
+		return "", fmt.Errorf("cannot read file %q: %w", orig, err)
+	}
+
+	return string(bytes), nil
+}
+
+// Search returns true if a file's body contains a substring.
+func Search(orig, term string) (bool, error) {
+	if !Exists(orig) {
+		return false, fmt.Errorf("cannot search file %q: does not exist", orig)
+	}
+
+	bytes, err := os.ReadFile(orig)
+	if err != nil {
+		return false, fmt.Errorf("cannot search file %q: %w", orig, err)
+	}
+
+	body := strings.ToLower(string(bytes))
+	term = strings.ToLower(term)
+	return strings.Contains(body, term), nil
+}
+
+// Update overwrites an existing file's body with a string.
+func Update(orig, body string, mode fs.FileMode) error {
+	if !Exists(orig) {
+		return fmt.Errorf("cannot update file %q: does not exist", orig)
+	}
+
+	if err := os.WriteFile(orig, []byte(body), mode); err != nil {
+		return fmt.Errorf("cannot update file %q: %w", orig, err)
+	}
+
+	return nil
+}
---
+++ akesi/tools/file/file_test.go
@@ -0,0 +1,118 @@
+package file
+
+import (
+	"path/filepath"
+	"strings"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stvmln86/akesi/akesi/tools/test"
+)
+
+func TestCreate(t *testing.T) {
+	// setup
+	dest := filepath.Join(t.TempDir(), "alpha.extn")
+
+	// success
+	err := Create(dest, "body\n", 0666)
+	test.AssertFile(t, dest, "body\n")
+	assert.NoError(t, err)
+
+	// failure - already exists
+	err = Create(dest, "body\n", 0666)
+	test.AssertErr(t, err, "cannot create file .*: already exists")
+}
+
+func TestDelete(t *testing.T) {
+	// setup
+	orig := test.TempFile(t, "alpha.extn")
+	dest := strings.Replace(orig, ".extn", ".trash", 1)
+
+	// success
+	err := Delete(orig)
+	assert.NoFileExists(t, orig)
+	assert.FileExists(t, dest)
+	assert.NoError(t, err)
+
+	// failure - does not exist
+	err = Delete(orig)
+	test.AssertErr(t, err, "cannot delete file .*: does not exist")
+}
+
+func TestExists(t *testing.T) {
+	// setup
+	orig := test.TempFile(t, "alpha.extn")
+
+	// success - true
+	ok := Exists(orig)
+	assert.True(t, ok)
+
+	// success - false
+	ok = Exists("/nope")
+	assert.False(t, ok)
+}
+
+func TestList(t *testing.T) {
+	// setup
+	dire := test.TempDire(t)
+
+	// success
+	elems, err := List(dire, ".extn")
+	assert.Equal(t, []string{
+		filepath.Join(dire, "alpha.extn"),
+		filepath.Join(dire, "bravo.extn"),
+	}, elems)
+	assert.NoError(t, err)
+
+	// failure - does not exist
+	elems, err = List("/nope", ".extn")
+	assert.Empty(t, elems)
+	test.AssertErr(t, err, "cannot list directory .*: does not exist")
+}
+
+func TestRead(t *testing.T) {
+	// setup
+	orig := test.TempFile(t, "alpha.extn")
+
+	// success
+	body, err := Read(orig)
+	assert.Equal(t, "Alpha note.\n", body)
+	assert.NoError(t, err)
+
+	// failure - does not exist
+	body, err = Read("/nope")
+	assert.Empty(t, body)
+	test.AssertErr(t, err, "cannot read file .*: does not exist")
+}
+
+func TestSearch(t *testing.T) {
+	// setup
+	orig := test.TempFile(t, "alpha.extn")
+
+	// success - true
+	ok, err := Search(orig, "ALPHA")
+	assert.True(t, ok)
+
+	// success - false
+	ok, err = Search(orig, "NOPE")
+	assert.False(t, ok)
+
+	// failure - does not exist
+	ok, err = Search("/nope", "NOPE")
+	assert.False(t, ok)
+	test.AssertErr(t, err, "cannot search file .*: does not exist")
+}
+
+func TestUpdate(t *testing.T) {
+	// setup
+	orig := test.TempFile(t, "alpha.extn")
+
+	// success
+	err := Update(orig, "body\n", 0666)
+	test.AssertFile(t, orig, "body\n")
+	assert.NoError(t, err)
+
+	// failure - does not exist
+	err = Update("/nope", "body\n", 0666)
+	test.AssertErr(t, err, "cannot update file .*: does not exist")
+}
